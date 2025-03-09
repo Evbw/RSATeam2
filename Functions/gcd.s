8a8abf3dec383ee2c485c344198e343f82810373
evbw <sp25bcd8b7@rpi-buster-armhf>
2025-03-09 15:59:38 +0000

    Added logic to test for different values

--- Functions/gcd.s
+++ Functions/gcd.s
@@ -29,16 +29,42 @@ main:
 
 	CMP r6, r5
 	BEQ Equal
+
+	CMP r6, r5
+	MOVGT r3, r6
+	CMP r5, r6
+	MOVLT r4, r5
 	
-	ContinueProgram:
+	CMP r6, r5
+	MOVLT r4, r6
+	CMP r5, r6
+	MOVGT r3, r5
 
 	StartGCDLoop:
+		CMP r3, r4
+		BEQ EndGCDLoop
+
+		SUB r2, r3, r4
+		CMP r2, r4
+		MOVGT r3, r2
+		CMP r2, r4
+		MOVLT r4, r3
+		CMP r2, r4
+		MOVLT r4, r2		
 	
 	EndGCDLoop:
+		MOV r1, r3
+		MOV r2, #1
+		CMP r1, r2
+		BEQ NoGCD
 
 	Equal:
 		MOV r1, r6
 		LDR r0, =equalOutput
+		BL printf
+	
+	NoGCD:
+		LDR r0, =noCommon
 		BL printf
 
 	LDR lr, [sp, #0]
@@ -49,6 +75,7 @@ main:
 	prompt1: .asciz "Please enter the first term\n"
 	prompt2: .asciz "Please enter the second term\n"
 	equalOutput: .asciz "The values are the same: %d\n"
+	noCommon: .asciz "Resulted in %d. There is no GCD.\n"
 	input: .asciz "%d"
 	num1: .word 0
 	num2: .word 0
