0fa01c0a51c5c3cc33d63861a67528e8a903086b
CBWright102 <cwrig102@jh.edu>
2025-03-24 19:42:20 -0500

    Update Encrypt.s
    Fixed Comments

--- Encrypt.s
+++ Encrypt.s
@@ -25,20 +25,20 @@ encrypt_loop:
 	
 	# Check if the exponent (r1) is zero
 	CMP r1, #0
-	BEQ encrypt_done					@ If the exponent is zero, then we're done
+	BEQ encrypt_done				@ If the exponent is zero, then we're done
 
 	# If the exponent (r1) is odd (r1 % 2 != 0), multiply the result by the base and take the modulo
-	AND r4, r1, #1						@ r4 = r1 & 1 (check if it's odd)
+	AND r4, r1, #1					@ r4 = r1 & 1 (check if it's odd)
 	CMP r4, #1
 	BEQ encrypt_odd_exponent
 
 	# If the exponent is even, just square the base and reduce the modulus
-	MUL r0, r0, r0						@ r0 = r0 * r0 (square the base)
+	MUL r0, r0, r0					@ r0 = r0 * r0 (square the base)
 	BL modulo						@ Call the modulo function
 	MOV r0, r0						@ Store the result of the modulo in r0 (base)
 
-	LSR r1, r1, #1						@ r1 = r1 / 2 (shift the exponent right by 1)
-	B encrypt_loop						@ Loop again
+	LSR r1, r1, #1					@ r1 = r1 / 2 (shift the exponent right by 1)
+	B encrypt_loop					@ Loop again
 
 .data
 @ ----- END FUNCTION 2 -----
@@ -48,16 +48,16 @@ encrypt_loop:
 .text
 encrypt_odd_exponent:
 	
-	MUL r3, r3, r0						@ r3 = r3 * base (multiply the result by the base)
+	MUL r3, r3, r0					@ r3 = r3 * base (multiply the result by the base)
 	BL modulo						@ Call the modulo function
 	MOV r3, r0						@ Store the result of the modulo in r3 (result)
 
-	MUL r0, r0, r0						@ r0 = r0 * r0 (square the base)
+	MUL r0, r0, r0					@ r0 = r0 * r0 (square the base)
 	BL modulo						@ Call the modulo function
 	MOV r0, r0						@ Store the result of the modulo in r0 (base)
 
-	LSR r1, r1, #1						@ r1 = r1 / 2 (shift the exponent right by 1)
-	B encrypt_loop						@ Loop again
+	LSR r1, r1, #1					@ r1 = r1 / 2 (shift the exponent right by 1)
+	B encrypt_loop					@ Loop again
 
 .data
 @ ----- END FUNCTION 3 -----
@@ -68,8 +68,8 @@ encrypt_odd_exponent:
 encrypt_done:
 	
 	# The result (cipher) is now in r3, so copy it to r0 (return value)
-	MOV r0, r3
-	BX lr							@ Return to the OS
+	mov r0, r3
+	bx lr							@ Return to the OS
 
 .data
 @ ----- END FUNTION 4 -----
