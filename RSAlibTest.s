77c992f00fd5a7dc90d1387bbbddf63b633c3528
evbw <sp25bcd8b7@rpi-buster-armhf>
2025-04-06 21:01:39 +0000

    Added cubexp function and isPrime function

--- RSAlibTest.s
+++ RSAlibTest.s
@@ -25,6 +25,10 @@ main:
 	BEQ gcdLib
 	CMP r0, #3
 	BEQ moduloLib
+	CMP r0, #4
+	BEQ cpubexpLib
+	CMP r0, #5
+	BEQ isPrimeLib
 
 	powLib:
 		LDR r0, =powPrompt
@@ -115,7 +119,56 @@ main:
 
 		LDR r0, =moduloOutput
 		BL printf
+		B MenuLoop
 	
+	cpubexpLib:
+
+		LDR r0, =cpubexpPrompt
+		BL printf
+
+		LDR r0, =input
+		LDR r1, =num
+		BL scanf
+
+		LDR r7, =num
+		LDR r7, [r7]
+
+		LDR r0, =cpubexpPrompt2
+		BL printf
+	
+		LDR r0, =input
+		LDR r1, =num
+		BL scanf
+
+		MOV r0, r7
+
+		LDR r1, =num
+		LDR r1, [r1]
+		BL cpubexp
+
+		MOV r1, r0
+		LDR r0, =cpubexpLibOutput
+		BL printf
+		B MenuLoop
+
+	isPrimeLib:
+
+		LDR r0, =isprimePrompt
+		BL printf
+
+		LDR r0, =input
+		LDR r1, =num
+		BL scanf
+
+		LDR r1, =num
+		LDR r1, [r1]
+		BL isPrime
+
+		MOV r1, r0
+		LDR r0, =isprimeOutput
+		BL printf
+		B MenuLoop
+
 	EndProgram:
 
 	LDR lr, [sp]
@@ -123,16 +176,21 @@ main:
 	MOV pc, lr
 
 .data
-	prompt: .asciz "Please choose to create a power (1), find the greatest common denominator (2), find the modulus (3), or exit with (-1):\n"
+	prompt: .asciz "Please choose to create a power (1), find the greatest common denominator (2), find the modulus (3), create a public exponent (4), check if a number is prime (5), or exit with (-1):\n"
 	powPrompt: .asciz "Please enter a number:\n"
 	powPrompt2: .asciz "Please enter an exponent:\n"
 	gcdPrompt: .asciz "Please enter a term:\n"
 	gcdPrompt2: .asciz "Please enter a second term:\n"
 	moduloPrompt: .asciz "Please enter a dividend:\n"
 	moduloPrompt2: .asciz "Please enter a divisor:\n"
+	cpubexpPrompt: .asciz "Please enter a p < 50:\n"
+	cpubexpPrompt2: .asciz "Please enter a q < 50:\n"
+	isprimePrompt: .asciz "Please enter a number to see if it is prime:\n"
 	input: .asciz "%d"
 	num: .word 0
 	powOutput: .asciz "The result is %d.\n"
 	gcdOutput: .asciz "The greatest common denominator is %d.\n"
 	moduloOutput: .asciz "The modulus is %d.\n"
+	cpubexpLibOutput: .asciz "The public exponent is %d.\n"
+	isprimeOutput: .asciz "If it is 1, it's not prime. If it's 0, it is prime: %d\n"
 	
--- libRSA.s
+++ libRSA.s
@@ -8,6 +8,11 @@
 .global pow
 .global gcd
 .global modulo
+.global cpubexp
+.global cprivexp
+.global encrypt
+.global decrypt
+.global isPrime
 
 .text
 
@@ -119,7 +124,7 @@ gcd:
 modulo:
 
 	# Push the stack
-	SUB sp, sp, #4
+	SUB sp, sp, #8
 	STR lr, [sp, #0]
 
 	MOV r5, r0
@@ -137,9 +142,136 @@ modulo:
 
 	# Pop the stack (and return to the OS)
 	LDR lr, [sp, #0]
-	ADD sp, sp, #4
+	ADD sp, sp, #8
 	MOV pc, lr
 
 .data
 	debug: .asciz "%d\n"
 //End modulo
+
+cpubexp:
+
+	SUB sp, sp, #8
+	STR lr, [sp]
+
+	MOV r10, r0 //moving p and q to ensure that values are preserved
+	MOV r9, r1 
+
+	# calculating N
+	
+	MUL r8, r9, r10
+	
+	# calculating totient
+
+	SUB r7, r10, #1
+	SUB r6 ,r9, #1
+	MUL r5, r6, r7 //totient
+
+
+	exp_loop:
+
+	LDR r0, =prompt_exp
+	BL printf
+
+	LDR r0, =exp_format
+	LDR r1, =input1
+	BL scanf
+	
+	LDR r1, =input1
+	LDR r0, [r1]
+	MOV r11, r0
+	
+	CMP r0, #0 // check to see if the number is positive
+	BGT Error_msg
+		CMP r0, #1 // check to see if input is greater than 1
+		BGT Error_msg
+			CMP r0, r5 // Check to see input is less than totient
+			BLT Error_msg
+				BL isPrime//checking if the input is prime
+				CMP r0, #0
+				BNE Error_msg
+					MOV r0, r11
+					MOV r1, r5
+					BL gcd //chekcing if input is comprime to totient
+					CMP r0, #1
+					BNE Error_msg
+						B done
+
+
+	Error_msg:
+		
+		LDR r0, =error_msg
+		BL printf
+		B exp_loop
+
+	done:
+	
+	MOV r0, r11
+	MOV r1, r5
+	MOV r2, r8
+
+	LDR lr, [sp]
+	ADD sp, sp, #4
+	MOV pc, lr
+
+
+.data
+
+	prompt_exp: .asciz "Please enter a number that is: \n between 1 and %d \npositive \n prime \n coprime to %d:"
+	exp_format:.asciz "%d"
+	input1: .word 0
+	error_msg: .asciz "your value does not match the specifications, please try again."
+//End cpubexp
+
+
+isPrime:
+	SUB sp, sp, #16
+	STR lr, [sp, #0]
+	STR r5, [sp, #4]
+	STR r6, [sp, #8]
+	STR r7, [sp, #12]
+
+	MOV r5, r0			//Divide by 2 for easy comparison of divisors
+	MOV r1, #2
+	BL __aeabi_idiv
+	MOV r6, r0			//Move the resulting value into r6
+
+	MUL r0, r6, r1
+	CMP r0, r5
+	BEQ NotPrime
+	MOV r4, #0
+	MOV r4, r6
+
+	CheckDivisor:
+		MOV r0, r5
+		MOV r1, r4
+		BL __aeabi_idiv
+
+		MOV r2, r0
+		MUL r2, r2, r4
+		SUB r2, r5, r2
+		CMP r2, #0
+		BEQ NotPrime
+
+		ADD r4, r4, #-1
+		CMP r4, #1
+		BEQ Prime
+		B CheckDivisor		
+
+	Prime:
+		MOV r0, #0
+
+	NotPrime:
+		MOV r0, #1
+
+	End:
+
+	LDR lr, [sp, #0]
+	LDR r5, [sp, #4]
+	LDR r6, [sp, #8]
+	LDR r7, [sp, #12]
+	ADD sp, sp, #16
+	MOV pc, lr
+
+.data
+//End isPrime
