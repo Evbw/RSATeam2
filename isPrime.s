ff5bef576643a97fe622164f3c05ce369936c9b8
evbw <sp25bcd8b7@rpi-buster-armhf>
2025-04-07 17:38:12 +0000

    Commiting for pull request

--- isPrime.s
+++ isPrime.s
@@ -13,7 +13,15 @@ isPrime:
 	SUB sp, sp, #4
 	STR lr, [sp]
 
-	MOV r0, r9
+	LDR r0, =prompt
+	BL printf
+
+	LDR r0, =input
+	LDR r1, =num
+	BL scanf
+
+	LDR r0, =num
+	LDR r0, [r0]
 	MOV r1, #2//check if the number is less than 2
 	MOV r2, #2// counter
 		
@@ -32,31 +40,30 @@ isPrime:
 			SUB r7, r5, r0
 			
 			CMP r7, #0
-			BEQ error_2 //not prime
+			BEQ error_1 //not prime
 			
 
 			ADD r2, r2, #1
 			B prime_loop
 
 		y_prime:
-			LDR r0, #1
+			MOV r1, #1
 			B done
 
 		error_1:
-			LDR r0, #0
+			MOV r1, #0
 		
 	done:
+
+		LDR r0, =output
+		BL printf
 
 	LDR lr, [sp]
 	ADD sp, sp, #4
 	MOV pc, lr
 
 .data
-
-
-		
-		
-			
-	
-
-	
+	prompt: .asciz "Enter a number:\n"
+	input: .asciz "%d"
+	num: .word 0
+	output: .asciz "%d"
