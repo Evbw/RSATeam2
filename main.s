b5063b8464d1c772e8d0a316cf6f2d3194765cb8
calvintang67 <calvintang67@gmail.com>
2025-04-12 23:24:42 +0000

    Modified p check algorithm

--- main.s
+++ main.s
@@ -7,13 +7,24 @@
 .text
 .global main
 main:
+    // Program dictionary:
+    // r4 - 
+    // r5 - 
+    // r6 -
+    // r7 -
+    // r8 -
+    // r9 - 
+    // r10 -
+    // r11 -
+    // r12 -
+
     // push stack record
     SUB sp, sp, #4
     STR lr, [sp]
 
     // Request p, q from Receiver; ensure p and q meet requirements
     // Loop for p value. p must be prime and < 50.
-    p_Loop:
+    p_StartLoop:
 
         LDR r0, =prompt1
         BL printf
@@ -23,36 +34,29 @@ main:
 
         // Verify 0 < p < 50
         LDR r0, =pValue
-        LDR r9, [r0]// putting the value in r9 to preserve the input
-	// BL isPrime //calling a function check if the number is prime
-	CMP r0, #0
-	BNE p_elsif2
-		LDR r0, =p_ErrorMsg2
-		BL printf
-		B p_Loop
-
-	p_elsif2:
-	
-        CMP r9, #0
-        BGT p_elsif1
-            // Statement if p <= 0
-            LDR r0, =p_ErrorMsg1
-            BL printf
-            B p_Loop
-        p_elsif1:
-            // Statement if 0 < p < 50
-            CMP r9, #50
-            BGE p_else
-            B endIf1
-        p_else:
-            LDR r0, =p_ErrorMsg1
-            BL printf
-            B p_Loop
-        endIf1:
+        LDR r0, [r0]
+        MOV r1, #0
+        CMP r0, #0
+	ADDGE r1, r1, #1  // p >= 0
+        MOV r2, #0
+        CMP r0, #50
+        ADDLT r2, r2, #1  // 0 <= p < 50
+        AND r1, r1, r2
 
         // Verify p is prime
-        
+        BL isPrime
+        AND r0, r0, r1
 
+        CMP r0, #1
+        BNE p_error
+            // Statement if p is valid
+            B p_EndLoop
+        p_error:
+            LDR r0, =p_ErrorMsg1
+            BL printf
+            B p_StartLoop
+        p_EndLoop:
+        
         LDR r0, =debug1
         LDR r1, =pValue
         LDR r1, [r1]
@@ -265,8 +269,8 @@ closeFile_error:
     pValue: .word 0
     qValue: .word 0
     decryptInput: .word 100
-    p_ErrorMsg1: .asciz "Invalid p value. Requirement: 0 < p < 50.\n"
-    q_ErrorMsg1: .asciz "Invalud q value. Requirement: 0 < q < 50.\n"
+    p_ErrorMsg1: .asciz "Invalid p value. Requirement: 0 <= p < 50, and must be prime.\n"
+    q_ErrorMsg1: .asciz "Invalud q value. Requirement: 0 <= q < 50, and must be prime.\n"
     debug1: .asciz "Valid p value: %d.\n"
     debug2: .asciz "Valid q value: %d.\n"
     debug: .asciz "Valid p value: %d.\n"
