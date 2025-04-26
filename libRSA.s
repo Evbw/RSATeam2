f76ebbd0852b37925ac2354876ab9176ab1c8a75
CBWright102 <cwrig102@jh.edu>
2025-04-26 01:10:26 -0500

    Update libRSA.s
    Updated Push/Pop for Modulo.

--- libRSA.s
+++ libRSA.s
@@ -142,29 +142,25 @@ gcd:
 modulo:
  
 # Push the stack
-    SUB sp, sp, #8
-    STR lr, [sp, #0]
-    STR r1, [sp, #4]
+	PUSH {r1, r5, lr}		// Save r1, r5, and the link register
 
 # Perform division
-    MOV r5, r0            // Save dividend
-    BL __aeabi_idiv       // Call division (quotient in r0)
+	MOV r5, r0			// Save the dividend
+	BL __aeabi_idiv			// Call the division (quotient in r0)
 
 # Store the quotient
-    MOV r2, r0            // r2 = quotient
+	MOV r2, r0			// r2 = quotient
 
 # Multiply the quotient by the divisor
-    MOV r3, r2            // Ensure different register for MUL
-    MUL r2, r3, r1        // r2 = quotient * divisor
+	MOV r3, r2			// Ensure different registers for MUL
+	MUL r2, r3, r1			// r2 = quotient * divisor
 
 # Subtract to get the remainder
-    SUB r0, r5, r2        // r0 = dividend - (quotient * divisor)
+	SUB r0, r5, r2			// r0 = dividend - (quotient * divisor)
 
 # Pop the stack (and return to the OS)
-    LDR lr, [sp, #0]
-    LDR r1, [sp, #4]
-    ADD sp, sp, #8
-    MOV pc, lr
+	POP {r1, r5, lr}		// Restore r1, r5, and the return address
+	MOV pc, lr
 
 .data
 //End modulo
