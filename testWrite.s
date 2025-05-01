4a8c851be5f06682f0c27f302dc0f60f8c937dfe
calvintang67 <calvintang67@gmail.com>
2025-05-01 10:28:22 +0000

    slakdma

--- testWrite.s
+++ testWrite.s
@@ -37,6 +37,53 @@ main:
     	bl fclose
 
     	mov r0, #0
+////////////////
+
+    	// fopen("encrypted.txt", "r")
+    	ldr r0, =infile
+    	ldr r1, =r_mode
+    	bl fopen
+    	ldr r1, =in_fp
+    	str r0, [r1]
+
+    	// fopen("plaintext.txt", "w")
+    	ldr r0, =outfile
+    	ldr r1, =w_mode
+    	bl fopen
+    	ldr r1, =out_fp
+    	str r0, [r1]
+
+	read_loop:
+
+    		// fgets(buffer, 256, in_fp)
+    		ldr r0, =buffer
+    		mov r1, #256
+		ldr r2, =in_fp
+    		ldr r2, [r2]
+    		bl fgets
+    		cmp r0, #0
+    		beq done
+
+    		// fprintf(out_fp, buffer)
+		ldr r0, =out_fp
+    		ldr r0, [r0]
+    		ldr r1, =buffer
+    		bl fprintf
+    		b read_loop
+
+	done:
+
+    		// fclose(in_fp)
+		ldr r0, =in_fp
+    		ldr r0, [r0]
+    		bl fclose
+
+    		// fclose(out_fp)
+		ldr r0, =out_fp
+    		ldr r0, [r0]
+    		bl fclose
+
+    		mov r0, #0
 
 	// pop stack
 	LDR lr, [sp, #0]
@@ -51,8 +98,13 @@ main:
 	msg1:      .asciz "This is a secret message.\n"
 	format: .asciz "%d"
 	msg2:      .word 0
+	buffer:   .space 256
+	infile:   .asciz "encrypted.txt"
+	outfile:  .asciz "plaintext.txt"
+	r_mode:   .asciz "r"
 
 .bss
 
 	fp: .skip 4     @ file pointer (32-bit)
-
+	in_fp:  .skip 4
+	out_fp: .skip 4
